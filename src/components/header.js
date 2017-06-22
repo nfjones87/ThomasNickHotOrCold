f9da6e7f7136ee3ad447dc002c787310481b541f
tckastanek <tckastanek@gmail.com>
2017-06-22 14:34:29 -0400

    toggling modal works

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,29 @@ import InfoModal from './info-modal';
 
 import './header.css';
 
-export default function Header(props) {
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNotDisplayed: true
+    };
+  }
+
+  toggleModal() {
+    if (this.state.isNotDisplayed) {
+      this.setState({ isNotDisplayed: false });
+    } else {
+      this.setState({ isNotDisplayed: true });
+    }
+  }
+
+  render() {
     return (
-        <header>
-            <TopNav />
-            {/* <InfoModal /> */}
-            <h1>HOT or COLD</h1>
-        </header>
+      <header>
+        <TopNav onClick={() => this.toggleModal()} />
+        <InfoModal isNotDisplayed={this.state.isNotDisplayed} onClick={() => this.toggleModal()} />
+        <h1>HOT or COLD</h1>
+      </header>
     );
-};
+  }
+}
--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -3,21 +3,20 @@ import React from 'react';
 import './top-nav.css';
 
 export default function TopNav(props) {
-    return (
-        <nav>
-            <ul className="clearfix">
-                <li>
-                    <a className="what" href="#">
-                        What?
+  return (
+    <nav>
+      <ul className='clearfix'>
+        <li>
+          <a className='what' href='#' onClick={() => props.onClick()}>
+            What?
                     </a>
-                </li>
-                <li>
-                    <a className="new" href="#">
+        </li>
+        <li>
+          <a className='new' href='#'>
                         + New Game
                     </a>
-                </li>
-            </ul>
-        </nav>
-    );
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
