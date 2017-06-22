692c449ffd07aeb2484369c6e94cc4a6d49a3a3e
tckastanek <tckastanek@gmail.com>
2017-06-22 17:13:59 -0400

    added number validation

--- src/components/game.js
+++ src/components/game.js
@@ -7,61 +7,63 @@ import GuessList from './guess-list';
 
 
 export default class Game extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            guesses: [],
-            num: Math.floor((Math.random() * 100) + 1),
-            feedback: 'Make your guess!'
-        }
+  constructor(props){
+    super(props);
+    this.state = {
+      guesses: [],
+      num: Math.floor((Math.random() * 100) + 1),
+      feedback: 'Make your guess!'
+    };
+  }
+
+  importGuesses(guess) {
+
+    if (this.state.guesses.includes(guess)) {
+      return this.setState({ feedback: 'You already guessed that.' });
+    } else if (guess < 1 || guess > 100) {
+      return this.setState({ feedback: 'Please choose a number between 1 and 100.'});
+    }{
+      this.setState({
+        guesses: [...this.state.guesses, guess]
+      });
     }
 
-    importGuesses(guess) {
+    const difference = Math.abs(guess - this.state.num);
 
-        if (this.state.guesses.includes(guess)) {
-          return this.setState({ feedback: 'You already guessed that.' });
-        } else {
-          this.setState({
-              guesses: [...this.state.guesses, guess]
-          });
-        }
-
-        const difference = Math.abs(guess - this.state.num);
-
-        if (Number(guess) === this.state.num) {
-          this.setState({ feedback: 'You got it...Great Job!' });
-        } else if (difference >= 50) {
-          this.setState({ feedback: 'You are ice cold!!!!' });
-        } else if (difference >= 30) {
-          this.setState({ feedback: 'You are pretty cold.' });
-        } else if (difference >= 10) {
-          this.setState({ feedback: 'Now you are warm.' });
-        } else if (difference >= 1) {
-          this.setState({ feedback: 'You are hot!' });
-        }
+    if (Number(guess) === this.state.num) {
+      this.setState({ feedback: 'You got it...Great Job!' });
+    } else if (difference >= 50) {
+      this.setState({ feedback: 'You are ice cold!!!!' });
+    } else if (difference >= 30) {
+      this.setState({ feedback: 'You are pretty cold.' });
+    } else if (difference >= 10) {
+      this.setState({ feedback: 'Now you are warm.' });
+    } else if (difference >= 1) {
+      this.setState({ feedback: 'You are hot!' });
     }
+  }
 
 
-    newGame() {
+  newGame() {
 
-        this.setState({
-            guesses: [],
-            num: Math.floor((Math.random() * 100) + 1)
-        })
+    this.setState({
+      guesses: [],
+      num: Math.floor((Math.random() * 100) + 1)
+    });
 
-    }
+  }
 
 
 
-    render() {
-        return (
-            <div>
-              <Header newGame={() => this.newGame()}/>
-              <GuessSection feedback={this.state.feedback} onGuess ={(guess) => this.importGuesses(guess)} />
-                <GuessCount count={this.state.guesses.length} />
-                <GuessList guesses={this.state.guesses} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header newGame={() => this.newGame()} />
+        <GuessSection feedback={this.state.feedback} onGuess={(guess) => this.importGuesses(guess)} />
+        <GuessCount count={this.state.guesses.length} />
+        <GuessList guesses={this.state.guesses} />
+      </div>
+    );
+  }
 
 }
